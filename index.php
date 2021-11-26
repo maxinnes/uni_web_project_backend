742d5114fe6f1db452e0e96e6aad60e020a507e2
Max <26414444+maxinnes@users.noreply.github.com>
2021-11-26 13:54:21 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
 $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
 
 foreach(array_keys($age) as $example){
-    echo $example;
+    echo $example."-";
 }
 
 echo "<br>";
+echo "<br>";
 
 foreach(array_values($age) as $example){
-    echo $example;
+    echo $example."-";
 }
