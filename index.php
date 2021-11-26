4386525b2d29f6c48e2086aad2ce6c6596375de7
Max <26414444+maxinnes@users.noreply.github.com>
2021-11-26 14:19:56 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
 // TODO Database should have email verification
 
-$age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+//$age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+//
+//foreach(array_keys($age) as $example){
+//    echo $example."-";
+//}
+//
+//echo "<br>";
+//echo "<br>";
+//
+//foreach(array_values($age) as $example){
+//    echo $example."-";
+//}
 
-foreach(array_keys($age) as $example){
-    echo $example."-";
-}
+$salt = openssl_random_pseudo_bytes(16);
+$hash = hash_pbkdf2("sha256", "Simple_123", $salt, 1000, 20);
 
-echo "<br>";
-echo "<br>";
+print($salt);
 
-foreach(array_values($age) as $example){
-    echo $example."-";
-}
+print($hash);
