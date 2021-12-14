40214ff8596399bbf39d6d8475dd985c5f8bc46f
Max <26414444+maxinnes@users.noreply.github.com>
2021-12-14 21:21:31 +0000

    Update verifyEmailAddress.php

--- api/email/verifyEmailAddress.php
+++ api/email/verifyEmailAddress.php
@@ -10,14 +10,18 @@ header('Content-type: application/json');
 $verificationCode =  $_GET['vc'];
 
 $verificationRecord = EmailVerification::getNewEmailVerificationByCode($verificationCode);
-try {
-    $verificationRecord = $verificationRecord->validateEmailAddress($verificationCode);
-    $isEmailValid = $verificationRecord->isEmailVerified();
-    if($isEmailValid){
-        echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_SUCCESSFUL,"Email address verified");
-    }else{
-        echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_FAIL,"Email was not validated.");
+if(!$verificationRecord->isEmailVerified()) {
+    try {
+        $verificationRecord = $verificationRecord->validateEmailAddress($verificationCode);
+        $isEmailValid = $verificationRecord->isEmailVerified();
+        if ($isEmailValid) {
+            echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_SUCCESSFUL, "Email address verified");
+        } else {
+            echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_FAIL, "Email was not validated.");
+        }
+    } catch (EmailValidationException $e) {
+        echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_FAIL, $e->getMessage());
     }
-} catch (EmailValidationException $e) {
-    echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_FAIL,$e->getMessage());
+}else{
+    echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_FAIL,"Email already verified.");
 }
