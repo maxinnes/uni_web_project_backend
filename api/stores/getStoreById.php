efc17d04015b0101d7ba6259f74b37953219a286
Max <26414444+maxinnes@users.noreply.github.com>
2021-12-25 20:52:38 +0000

    Removed need for login to use store api

--- api/stores/getStoreById.php
+++ api/stores/getStoreById.php
@@ -9,9 +9,5 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 $storeId = $_GET['id'];
 
-if(isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn']==true){
-    $storeObj = new Stores($storeId);
-    echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_SUCCESSFUL,"Returned details",$storeObj->returnAsAssocArray());
-}else{
-    echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_FAIL,"User not logged in");
-}
+$storeObj = new Stores($storeId);
+echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_SUCCESSFUL,"Returned details",$storeObj->returnAsAssocArray());
--- api/stores/getStoreByUrl.php
+++ api/stores/getStoreByUrl.php
@@ -10,18 +10,14 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 $url = $_GET['url'];
 
-if(isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn']==true){
-    $storeObj = Stores::getStoreByUrl($url);
-    $listOfProductObjs = StoreProducts::getProductsByStoreId($storeObj->id);
-    $listOfProducts = array();
-    foreach($listOfProductObjs as $productObj){
-        $listOfProducts[] = $productObj->returnAsAssocArray();
-    }
-    $returnObj = array(
-        "storeDetails"=>$storeObj->returnAsAssocArray(),
-        "productDetails"=>$listOfProducts
-    );
-    echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_SUCCESSFUL,"Returned details",$returnObj);
-}else{
-    echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_FAIL,"User not logged in");
+$storeObj = Stores::getStoreByUrl($url);
+$listOfProductObjs = StoreProducts::getProductsByStoreId($storeObj->id);
+$listOfProducts = array();
+foreach($listOfProductObjs as $productObj){
+    $listOfProducts[] = $productObj->returnAsAssocArray();
 }
+$returnObj = array(
+    "storeDetails"=>$storeObj->returnAsAssocArray(),
+    "productDetails"=>$listOfProducts
+);
+echo JsonServerResponse::createJsonResponse(JsonServerResponse::MESSAGE_SUCCESSFUL,"Returned details",$returnObj);
